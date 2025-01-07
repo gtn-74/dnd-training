86318b6572c318cfefa1776ae7c79b375e09c673
gtn-74 <gtn.r74@gmail.com>
2025-01-08 00:11:07 +0900

    study:theme

--- src/page/index.tsx
+++ src/page/index.tsx
@@ -1,7 +1,24 @@
-import React from 'react'
+import { Box, styled } from "@mui/material";
+
+import React from "react";
 
 export default function Index() {
-  return (
-    <div>index</div>
-  )
+  return <StyleBox>index</StyleBox>;
 }
+
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+const StyleBox = styled(Box)(({ theme }) => ({
+  // backgroundColor: theme.palette.primary.main, // テーマの primary 色を使用
+  // borderRadius: theme.shape.borderRadius,     // テーマのボーダー半径を使用
+
+  // themeで分岐を作って当てるスタイルを変更する
+  [theme.breakpoints.only("xs")]: {
+    padding: theme.spacing(2), // `xs` の場合の padding
+  },
+  [theme.breakpoints.up("sm")]: {
+    padding: theme.spacing(4), // `sm` 以上の場合の padding
+  },
+
+  
+
+}));
--- src/provider/ThemeProvider.tsx
+++ src/provider/ThemeProvider.tsx
@@ -2,6 +2,7 @@ import { createTheme, ThemeProvider as Provider } from "@mui/material/styles";
 
 // カスタムテーマの作成
 const theme = createTheme({
+  
   palette: {
     primary: {
       50: "blue",
@@ -24,13 +25,47 @@ const theme = createTheme({
   },
 });
 
-interface ThemeType {
-  children: React.ReactNode;
-}
+
+// font.ts
+theme.typography.h3 = {
+  fontSize: '1.2rem',
+  '@media (min-width:600px)': {
+    fontSize: '1.5rem',
+  },
+  [theme.breakpoints.up('md')]: {
+    fontSize: '2.4rem',
+  },
+};
+
+// 使う時、
+// Typograhyのvariantに、このスタイルが割り当てられるっぽい
+
+// !
+// const customSpacing = (base: number) => (factor: number) => factor * base;
+
+// const theme = createTheme({
+//   customSpacing: {
+//     cardSpacing: customSpacing(12),
+//     sectionMargin: customSpacing(16),
+//   },
+// });
+
+// // 使用例
+// const StyledBox = styled(Box)(({ theme }) => ({
+//   padding: theme.customSpacing.cardSpacing(3), // 値は 36 (12 * 3)
+//   margin: theme.customSpacing.sectionMargin(2), // 値は 32 (16 * 2)
+// }));
+
+
+
+
+
+
+
 
 console.log(theme.palette.primary.main, 50);
 
-export default function ThemeProvider({ children }: ThemeType) {
+export default function ThemeProvider({ children }: React.PropsWithChildren) {
   return <Provider theme={theme}>{children}</Provider>;
 }
 
