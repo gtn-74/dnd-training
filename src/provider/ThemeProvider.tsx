2890a54018d10dd481cc3aae9cb915c438985f73
gtn-74 <gtn.r74@gmail.com>
2025-01-12 21:13:22 +0900

    feat:new theme

--- src/provider/ThemeProvider.tsx
+++ src/provider/ThemeProvider.tsx
@@ -1,13 +1,11 @@
-import { createTheme, ThemeProvider as Provider } from "@mui/material/styles";
+import {
+  createTheme,
+  ThemeProvider as Provider,
+  Theme,
+} from "@mui/material/styles";
 
 // カスタムテーマの作成
 const theme = createTheme({
-  
-  palette: {
-    primary: {
-      50: "blue",
-    },
-  },
   components: {
     MuiSwitch: {
       styleOverrides: {
@@ -25,22 +23,22 @@ const theme = createTheme({
   },
 });
 
-
 // font.ts
 theme.typography.h3 = {
-  fontSize: '1.2rem',
-  '@media (min-width:600px)': {
-    fontSize: '1.5rem',
+  fontSize: 1.2,
+  p: 5,
+  "@media (min-width:600px)": {
+    fontSize: "1.5rem",
   },
-  [theme.breakpoints.up('md')]: {
-    fontSize: '2.4rem',
+  [theme.breakpoints.up("md")]: {
+    fontSize: "2.4rem",
   },
 };
 
 // 使う時、
 // Typograhyのvariantに、このスタイルが割り当てられるっぽい
 
-// !
+// ! MUIのTypographyを使いこなしてみる
 // const customSpacing = (base: number) => (factor: number) => factor * base;
 
 // const theme = createTheme({
@@ -56,14 +54,7 @@ theme.typography.h3 = {
 //   margin: theme.customSpacing.sectionMargin(2), // 値は 32 (16 * 2)
 // }));
 
-
-
-
-
-
-
-
-console.log(theme.palette.primary.main, 50);
+// console.log(theme.palette.primary.main, 50);
 
 export default function ThemeProvider({ children }: React.PropsWithChildren) {
   return <Provider theme={theme}>{children}</Provider>;
@@ -72,28 +63,14 @@ export default function ThemeProvider({ children }: React.PropsWithChildren) {
 // import { createTheme, ThemeProvider as Provider } from "@mui/material/styles";
 
 // // カスタムテーマの作成
-// const theme = createTheme({
-//   palette: {
-//     secondary: {
-//       main: "#ff5722", // secondaryのデフォルト色
-//     },
-//   },
-//   components: {
-//     MuiSwitch: {
-//       styleOverrides: {
-//         // secondaryのスタイルをカスタマイズ
-//         colorSecondary: {
-//           "&.Mui-checked": {
-//             color: "blue", // チェックされた状態のアイコンの色
-//           },
-//           "&.Mui-checked + .MuiSwitch-track": {
-//             backgroundColor: "blue", // チェックされた状態のスライダー背景色
-//           },
-//         },
-//       },
-//     },
-//   },
-// });
+// eslint-disable-next-line react-refresh/only-export-components
+export const secondaryTheme: Theme = createTheme({
+  palette: {
+    secondary: {
+      main: "#ff5722", // secondaryのデフォルト色
+    },
+  },
+});
 
 // interface ThemeType {
 //   children: React.ReactNode;
